68515a082e92cf22d72a93f8a8bb6785ce0ded25
Selen <selenyanar@gmail.com>
2022-12-04 10:11:23 +0300

    Initiate project & create simple UI

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var pickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
